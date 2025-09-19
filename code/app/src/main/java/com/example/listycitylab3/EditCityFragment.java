a49fa2619f11cad62183a1e4af0a92a84f307cc5
Dengngut <ngut@ualberta.ca>
2025-09-18 21:24:35 -0600

    lab-03

--- code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -1,4 +1,75 @@
 package com.example.listycitylab3;
 
-public class EditCityFragment {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+    interface EditCityDialogListener {
+        void update(City city, int position);
+    }
+    private EditCityDialogListener listener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof EditCityDialogListener) {
+            listener = (EditCityDialogListener) context;
+            } else {
+            throw new RuntimeException(context + " must implement EditCityDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+
+        EditText cityEdit = view.findViewById(R.id.edit_text_city_text);
+        EditText provinceEdit = view.findViewById(R.id.edit_text_province_text);
+
+        Bundle args = requireArguments();
+        City city = (City) args.getSerializable("city");
+       int position = args.getInt("position");
+
+
+        cityEdit.setText(city.getName());
+        provinceEdit.setText(city.getProvince());
+
+
+        return new AlertDialog.Builder(getContext())
+                .setView(view)
+                .setTitle("Edit City")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Save", (dialog, which) -> {
+                    city.setName(cityEdit.getText().toString());
+                    city.setProvince(provinceEdit.getText().toString());
+                    listener.update(city, position);
+                })
+                .create();
+    }
+
+
+    public static EditCityFragment newInstance(City city, int position) {
+        Bundle args = new Bundle();
+        args.putSerializable("city", city);
+        args.putInt("position", position);
+        EditCityFragment fragment = new EditCityFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+
 }
+
+
